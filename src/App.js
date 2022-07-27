291fce6aacb261998faa1ef9c4c7181165805940
Sinara Moura <sinara1295@hotmail.com>
2022-07-26 21:06:07 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import profile from './assets/profile.jpg'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <div className="container">
+      <header>
+        <h2>Nara.dev</h2>
+        <ul className="menu">
+          <li>Artigos</li>
+          <li>Contato</li>
+        </ul>
       </header>
+
+      <section>
+        <div className="left">
+          <img src={profile} alt="sinara" />
+          <strong>Sinara Tibel</strong>
+          <span>
+            <br />
+            Eu sou o Sinara , estudante fullstack na
+            Cubos Academy, com foco em ser desenvolvedora frontend.
+            <br />
+            Sou apaixonada por tecnologia!
+          </span>
+        </div>
+        <div className="right">
+          <strong>22 de Novembro de 2030</strong>
+          <h2>Introdução ao React</h2>
+          <span>Nesse post nós iremos falar sobre como o React,
+            é importante para o desenvolvedor frontend moderno.
+          </span>
+          <a href="#">Ler mais</a>
+        </div>
+      </section>
+
+      <footer>
+        <ul className="menu">
+          <li>Artigos</li>
+          <li>Contato</li>
+        </ul>
+      </footer>
     </div>
+
   );
 }
 
-export default App;
